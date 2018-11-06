281cbcd975395724804a079a71bddd2cd872572b
angeliquecd <acharles-davis00@stuy.edu>
2018-11-06 11:00:44 -0500

    filled in columnsum

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -13,6 +13,13 @@ public class ArrayMethods{
     //returns the sum of the elements in Row x of ary.
     //When x is past the last row count it as a zero. (NO indexOutOfBounds should ever occur)
 
-   public static int columnSum(int[][] ary, int x){}
+   public static int columnSum(int[][] ary, int x){
+     int sum=0;
+     for (int i=0;i<ary.length;i++){
+       sum+=ary[i][x];
+     }
+     return sum;
+   }
+   }
     //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
     //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
