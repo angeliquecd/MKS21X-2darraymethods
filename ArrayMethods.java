128fec2d60bfe6f9d7d0fa060db6a98bcc173b75
angeliquecd <acharles-davis00@stuy.edu>
2018-11-06 11:03:22 -0500

    filled in allrowsums

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -20,6 +20,26 @@ public class ArrayMethods{
      }
      return sum;
    }
+   *PART 2 - use prior methods where appropriate
+  */
+  public static int[] allRowSums(int[][] ary){
+    int[] newy= new int[ary.length];
+    (for int i =0;i<ary.length;i++){
+      newy[i]=rowSum(ary,i);
+    }
+    return newy;
+  }
+   //returns an array of the row sums for each row of ary.
+   //Index i of the return array contains the sum of elements in row i.
+
+   public static int[] allColSums(int[][] ary){}
+   //Returns an array with the column sum of each column of ary.
+   //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
+   //Index i of the return array contains the sum of elements in column i, ignoring any rows that are too short.
+   //The length of the returned array should be the length of the LONGEST array in ary.
+
+
+
    }
     //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
     //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
